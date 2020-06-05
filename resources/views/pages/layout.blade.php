69fb29c55861312db24cc0a3a76f5b1cc1c68912
Steve Idowu <idowusteve@ymail.com>
2020-06-05 11:01:37 +0100

    Fixing th edit button

--- resources/views/pages/layout.blade.php
+++ resources/views/pages/layout.blade.php
@@ -5,7 +5,7 @@
     <div class="w-full px-3">
       <h3 class="text-2xl font-semibold text-gray-700">Employees</h3>
     </div>
-    <div class="w-full lg:w-2/12 bg-white rounded-lg mx-2 my-2 shadow-md p-6">
+    <div class="w-full lg:w-3/12 bg-white rounded-lg mx-2 my-2 shadow-md p-6">
       <side-bar />
     </div>
     <div class="w-full lg:w-7/12 bg-white mx-2 rounded-lg my-2 shadow-md p-6">
